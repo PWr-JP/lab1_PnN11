e8c163a33e46dc9942aa7e851e6b76c2fcad9929
Tomasz Babczyński <tomasz.babczynski@pwr.edu.pl>
2021-10-18 12:25:17 +0200

    pierwsza dzialajaca wersja

--- src/tb/soft/Main.java
+++ src/tb/soft/Main.java
@@ -2,7 +2,14 @@ package tb.soft;
 
 public class Main {
 
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.println("Witaj");
+
+        System.exit(0);
     }
 }
